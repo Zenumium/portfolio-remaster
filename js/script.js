48764ebaadad6af38270854c3d7f02d2208a7159
vlouvier <louvier.valentyn@gmail.com>
2025-01-29 16:14:21 +0100

    js/

--- js/script.js
+++ js/script.js
@@ -40,23 +40,6 @@
 //   },
 // ];
 
-function renderProjects() {
-  const projectGrid = document.getElementById("projectGrid");
-  projects.forEach((project, index) => {
-    const card = document.createElement("div");
-    card.classList.add("project-card");
-    card.dataset.index = index;
-    card.innerHTML = `
-      <img alt="${project.title}" srcset="${project.image}">
-      <h3>${project.title}</h3>
-      <p>${project.description}</p>
-      <small>${project.type} Project</small>
-    `;
-    card.addEventListener("click", openModal);
-    projectGrid.appendChild(card);
-  });
-}
-
 const modal = document.getElementById("projectModal");
 const modalDetails = document.getElementById("modalDetails");
 const closeModal = document.querySelector(".close-modal");
